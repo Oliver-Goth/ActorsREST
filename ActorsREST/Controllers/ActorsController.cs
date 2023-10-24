6bc7be2265f9402378d662533ccb27baecaeb5cd
Oliver-Goth <Olivergoth00@gmail.com>
2023-10-24 10:20:26 +0200

    Add project files.

---
+++ ActorsREST/Controllers/ActorsController.cs
@@ -0,0 +1,100 @@
+﻿using ActorRepositoryLib;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ActorsREST.Controllers
+{
+    [Route("api/[controller]")]
+    [EnableCors(PolicyNames.OnlyZealand)]
+    [ApiController]
+    public class ActorsController : ControllerBase
+    {
+        private IActorsRepository _actorsRepository;
+
+        public ActorsController(IActorsRepository actorsRepository)
+        {
+            _actorsRepository = actorsRepository;
+        }
+
+        // GET: api/Actors
+        [HttpGet]
+        public ActionResult<IEnumerable<Actor>> Get(
+            [FromHeader] string? amount,
+            [FromQuery] string? nameFilter
+            )
+        {
+            int? parsedAmount = null;
+            if (amount != null)
+            {
+                if (int.TryParse(amount, out var tempParsedAmount))
+                {
+                    parsedAmount = tempParsedAmount;
+                } else
+                {
+                    return BadRequest("Amount must be a integer");
+                }
+            }
+
+            return Ok(_actorsRepository.Get(nameFilter,
+                null, null, parsedAmount));
+        }
+
+        // GET api/<ActorsController>/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status418ImATeapot)]
+        [EnableCors(PolicyNames.AllowAllPolicy)]
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<Actor> GetById([FromHeader] string color
+            , string id)
+        {
+            if (!int.TryParse(id, out int actorId))
+            {
+
+                Response.Headers.Add("Volume", "1l");
+                return StatusCode(StatusCodes.Status418ImATeapot);
+            }
+            Actor? actor = _actorsRepository.GetById(actorId);
+            if (actor == null) return NotFound();
+            return Ok(actor);
+        }
+
+        // POST api/<ActorsController>
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpPost]
+        public ActionResult<Actor> Post([FromBody] Actor newActor)
+        {
+            try
+            {
+                Actor addedActor = _actorsRepository.Add(newActor);
+                return Created("/" + addedActor.Id, addedActor);
+            }
+            catch (ArgumentNullException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // PUT api/<ActorsController>/5
+        [HttpPut("{id}")]
+        public Actor? Put(int id, [FromBody] Actor value)
+        {
+            return _actorsRepository.Update(id, value);
+        }
+
+        // DELETE api/<ActorsController>/5
+        [HttpDelete("{id}")]
+        public Actor? Delete(int id)
+        {
+            return _actorsRepository.Delete(id);
+        }
+    }
+}
---
+++ ActorsREST/Program.cs
@@ -0,0 +1,57 @@
+using ActorRepositoryLib;
+using ActorsREST;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddCors(options => {
+    options.AddPolicy(name: PolicyNames.AllowAllPolicy, policy =>
+    {
+        policy.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+    options.AddPolicy(name: PolicyNames.OnlyZealand, policy =>
+    {
+        policy.WithOrigins("http://zealand.dk"
+            , "https://zealand.dk")
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
+
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+bool useSql = false;
+if (useSql)
+{
+    var optionsBuilder = new DbContextOptionsBuilder<ActorsDbContext>();
+    ActorsDbContext context = new ActorsDbContext(optionsBuilder.Options);
+    builder.Services.AddSingleton<IActorsRepository>
+        (new ActorsRepositoryDB(context));
+}
+else
+{
+    builder.Services.AddSingleton<IActorsRepository>
+        (new ActorsRepositoryList());
+}
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseCors(PolicyNames.OnlyZealand);
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
